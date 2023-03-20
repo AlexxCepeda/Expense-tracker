3b845868d4d599b739f5580d601aa9ba356aece9
AlexxCepeda <acepeda1800@alumno.ipn.mx>
2023-03-20 16:18:45 -0600

    dummy expenses

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from './components/NewExpense/NewExpense';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 1, title: 'StepperMotor', amount: 219.22, date: new Date(2023, 11, 5)
-  }, {
-    id: 2, title: 'Threaded inserts', amount: 214.23, date: new Date(2021, 5, 12)
-  }, {
-    id: 3, title: 'Screws and nuts', amount: 121.32, date: new Date(2022, 8, 16)
-  }]
+// const DUMMY_EXPENSES = [
+//   {
+//     id: 1, title: 'StepperMotor', amount: 219.22, date: new Date(2023, 11, 5)
+//   }, {
+//     id: 2, title: 'Threaded inserts', amount: 214.23, date: new Date(2021, 5, 12)
+//   }, {
+//     id: 3, title: 'Screws and nuts', amount: 121.32, date: new Date(2022, 8, 16)
+//   }]
 function App() {
 
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const [expenses, setExpenses] = useState([]);
 
   const addExpenseHandler = expense => {
     setExpenses(prevExpenses => [expense, ...prevExpenses])
